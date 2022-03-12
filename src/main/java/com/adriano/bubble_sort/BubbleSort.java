e72f55845a94788ecdaba9bb517853991d1b28a2
Rafael Garbin <rafael.garbin@digibee.com>
2022-03-11 22:42:37 -0300

    inital commit

---
+++ src/main/java/com/adriano/bubble_sort/BubbleSort.java
@@ -0,0 +1,12 @@
+package com.adriano.bubble_sort;
+
+
+import java.util.List;
+
+public class BubbleSort {
+
+    public List<Integer> shortList(List<Integer> list) {
+        return null;
+    }
+
+}
---
+++ src/test/java/com/adriano/bubble_sort/SortTest.java
@@ -0,0 +1,59 @@
+package com.adriano.bubble_sort;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class SortTest {
+
+    private BubbleSort bubbleSort;
+
+    @Before
+    public void init() {
+        bubbleSort = new BubbleSort();
+    }
+
+    @Test
+    public void testShotWithNoElements() {
+        List<Integer> result = bubbleSort.shortList(Arrays.asList());
+        List<Integer> expected = Arrays.asList();
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testShotWhenListToShortIsNull() {
+        List<Integer> result = bubbleSort.shortList(null);
+        List<Integer> expected = Arrays.asList();
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testShotWithOneElements() {
+        List<Integer> result = bubbleSort.shortList(Arrays.asList(1));
+        List<Integer> expected = Arrays.asList(1);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testShotTwoElements() {
+        List<Integer> result = bubbleSort.shortList(Arrays.asList(2, 1));
+        List<Integer> expected = Arrays.asList(1, 2);
+
+        Assert.assertEquals(expected, result);
+    }
+
+    @Test
+    public void testShotTreeElements() {
+        List<Integer> result = bubbleSort.shortList(Arrays.asList(3, 2, 1));
+        List<Integer> expected = Arrays.asList(1, 2, 3);
+
+        Assert.assertEquals(expected, result);
+    }
+
+}
